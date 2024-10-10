fda6241374ca1208ec7bb402a10f30bd5d7c2e31
Raseen2004 <rajaraseen105@gmail.com>
2024-10-10 21:04:48 +0530

    Day 15

---
+++ 169MajorityEle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i])+1);
+            }else {
+                map.put(nums[i],1);
+            }
+        }
+        int maxV = Collections.max(map.values());
+        int maxK = 0;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() == maxV) {
+                maxK = entry.getKey();
+            }
+        }
+
+        return maxK;
+    }
+}
