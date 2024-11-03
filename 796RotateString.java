6d576c79954f60037425306bffdb538700b9f639
Raseen2004 <rajaraseen105@gmail.com>
2024-11-03 10:54:40 +0530

    Day 37

---
+++ 796RotateString.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length())
+            return false;
+        StringBuilder sb = new StringBuilder(s);
+        int l = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char firstChar = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(firstChar);
+            if (sb.toString().equals(goal)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
